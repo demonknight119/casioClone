90ce4ae2313b802177b1dbf2b6bf352a4c494f19
monukashyap5612 <monukashyap5612@gmail.com>
2025-02-09 23:00:53 +0530

    commit message

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example: Add to cart functionality
+const addToCartButtons = document.querySelectorAll('.product-card button');
+
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Added to cart!');
+  });
+});
